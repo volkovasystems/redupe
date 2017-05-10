168af4100b7f1a4a2f6ac7e8cfb50c02be65df35
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-10 11:22:32 +0800

    Updated test.

--- redupe-test.js
+++ redupe-test.js
@@ -1,10 +1,12 @@
 const assert = require( "assert" );
 const redupe = require( "./redupe.js" );
 
-assert.deepEqual( redupe( { "hello": { "world": "yeah", "weeh": 1234 } }, { "hello": { "world": 123, "us": "hi" } } ),
-{ "hello": { "world": 123, "weeh": 1234, "us": "hi" } } );
+assert.deepEqual( redupe( { "hello": { "world": "yeah", "weeh": 1234 } },
+{ "hello": { "world": 123, "us": "hi" } } ),
+{ "hello​.world": 123, "hello​.weeh": 1234, "hello​.us": "hi" } );
 
-assert.deepEqual( redupe( { "hello": { "world": "yeah", "weeh": 1234 } }, { "hello": { "world": 123, "us": "hi" } }, true ),
-{ "hello": { "world": "yeah", "weeh": 1234, "us": "hi" } } );
+assert.deepEqual( redupe( { "hello": { "world": "yeah", "weeh": 1234 } },
+{ "hello": { "world": 123, "us": "hi" } } ),
+{ "hello​.world": 123, "hello​.weeh": 1234, "hello​.us": "hi" } );
 
 console.log( "ok" );
