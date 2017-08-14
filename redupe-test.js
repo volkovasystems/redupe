305ff6e1a81f8db8b4df1d3bc3ffc380b238163a
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-14 16:17:15 +0800

    Updated test.

--- redupe-test.js
+++ redupe-test.js
@@ -2,11 +2,11 @@ const assert = require( "assert" );
 const redupe = require( "./redupe.js" );
 
 assert.deepEqual( redupe( { "hello": { "world": "yeah", "weeh": 1234 } }, { "hello": { "world": 123, "us": "hi" } } ),
-							{ "hello​.world": 123, "hello​.weeh": 1234, "hello​.us": "hi" },
-							"should be deeply equal" );
+	{ "hello​.world": 123, "hello​.weeh": 1234, "hello​.us": "hi" },
+	"should be equal to { 'hello​.world': 123, 'hello​.weeh': 1234, 'hello​.us': 'hi' }" );
 
 assert.equal( typeof redupe( { "hello": { "world": "yeah", "weeh": 1234 } },
-								{ "hello": { "world": 123, "us": "hi" } } ) == "object",
-						 		true, "should return true" );
+	{ "hello": { "world": 123, "us": "hi" } } ) == "object",
+	true, "should be of object data type" );
 
 console.log( "ok" );
